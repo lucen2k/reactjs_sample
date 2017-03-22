c9f62759a8b04e3a7599fa4770b43ac7eb3566c1
Lucen jeong <lucen2k@gmail.com>
2017-03-22 06:58:07 +0000

    #3 add home

--- sample2/render.js
+++ sample2/render.js
@@ -9,17 +9,37 @@ var Layout = React.createClass({
                     <li>Contect</li>
                 </ul>
                 <div className="content">
+                    <Home />
                 </div>
             </div>
         )
     }
 });
 
+var Home = React.createClass({
+   render: function() {
+        return (
+            <div>
+              <h2>HELLO</h2>
+              <p>Cras facilisis urna ornare ex volutpat, et
+              convallis erat elementum. Ut aliquam, ipsum vitae
+              gravida suscipit, metus dui bibendum est, eget rhoncus nibh
+              metus nec massa. Maecenas hendrerit laoreet augue
+              nec molestie. Cum sociis natoque penatibus et magnis
+              dis parturient montes, nascetur ridiculus mus.</p>
+      
+              <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+            </div>            
+        );
+   }
+});
+
 var Destination = document.querySelector("#contant");
 
 ReactDOM.render(
-  <div>
-    <Layout />
-  </div>,
+  <ReactRouter.Router history={ReactRouter.hashHistory}>
+    <ReactRouter.Route path="/" component={Layout}>
+    </ReactRouter.Route>
+  </ReactRouter.Router>,
   Destination
 );
