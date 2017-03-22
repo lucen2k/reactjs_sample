cda06098a2d34fee587fac05ef5515c4747d1f57
Lucen jeong <lucen2k@gmail.com>
2017-03-22 06:34:57 +0000

    #3 make layout

--- sample2/render.js
+++ sample2/render.js
@@ -1,8 +1,25 @@
+var Layout = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1>Simple SPA</h1>
+                <ul className="header">
+                    <li>Home</li>
+                    <li>Stuff</li>
+                    <li>Contect</li>
+                </ul>
+                <div className="content">
+                </div>
+            </div>
+        )
+    }
+});
+
 var Destination = document.querySelector("#contant");
 
 ReactDOM.render(
   <div>
-    SPA Start!!
+    <Layout />
   </div>,
   Destination
 );
